c4533f3e0f57b4f867cf725cdc4020d3aa05bd85
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-09-01 23:02:13 -0400

    inserting documents

--- monogodb.js
+++ monogodb.js
@@ -16,9 +16,62 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-      name: 'Davin',
-      age: 30,
-    });
+    // db.collection('users').insertOne(
+    //   {
+    //     name: 'Davin',
+    //     age: 30,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert user');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection('users').insertMany(
+    //   [
+    //     {
+    //       name: 'Janet',
+    //       age: 27,
+    //     },
+    //     {
+    //       name: 'Jack',
+    //       age: 29,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('An Error occurred while inserting.');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection('tasks').insertMany(
+      [
+        {
+          task: 'Get To Top 1% on CodeWars',
+          completed: false,
+        },
+        {
+          task: 'Become a Full Stack Developer',
+          completed: true,
+        },
+        {
+          task: 'Develop My Own Business',
+          completed: false,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log('Error while inserting documents');
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
