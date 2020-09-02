fdb7f8445340218d250369255e906f796cc1c57a
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-09-02 14:57:08 -0400

    update documents

---
+++ mongodb.js
@@ -0,0 +1,61 @@
+// CRUD - CREATE READ UPDATE DELETE
+
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require('mongodb');
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log('Unable to connect to database!');
+    }
+
+    const db = client.db(databaseName);
+
+    // db.collection('users')
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID('5f4f08020b0b47c60bb40862'),
+    //     },
+    //     {
+    //       $set: {
+    //         name: 'Jack',
+    //       },
+    //       $inc: {
+    //         age: 5,
+    //       },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    db.collection('tasks')
+      .updateMany(
+        {
+          completed: false,
+        },
+        {
+          $set: {
+            completed: true,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+);
