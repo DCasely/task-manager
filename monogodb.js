c8695957816b46b93f10aaa2646b21e4bd024e8a
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-09-01 22:31:28 -0400

    initial commit

---
+++ monogodb.js
@@ -0,0 +1,24 @@
+// CRUD - CREATE READ UPDATE DELETE
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log('Unable to connect to database!');
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+      name: 'Davin',
+      age: 30,
+    });
+  }
+);
