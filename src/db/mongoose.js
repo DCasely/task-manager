545396825b6e4583fd0a2060c81cecd626917ab8
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-09-03 17:03:24 -0400

    resource creation endpoints: part 2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,26 +5,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
-// const Task = mongoose.model('Task', {
-//   description: {
-//     type: String,
-//     trim: true,
-//     required: true,
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false,
-//   },
-// });
-
-// const newTask = new Task({
-//   description: 'GOT IT DONE?   ',
-// })
-//   .save()
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,7 +16,20 @@ app.post('/users', (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
     })
     .catch((e) => {
       res.status(400).send(e);
